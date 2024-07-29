fcba783a732dde480f1cca038acf8410116eabd0
prudhvi-1122 <prudhwi1998@gmail.com>
2024-07-29 17:01:14 +0530

    vault

--- variables.tf
+++ variables.tf
@@ -2,19 +2,24 @@ variable "tools" {
   default = {
 
     github-runner = {
-        instance_type = "t2.micro"
-        policy_name = [
-          "AdministratorAccess"
-        ]
+      instance_type = "t2.micro"
+      policy_name = [
+        "AdministratorAccess"
+      ]
+      ports = {}
     }
     vault = {
       instance_type = "t2.micro"
       policy_name = []
+      ports = {
+        vault = 8200
     }
+  }
+
+ }
 }    
 variable "hosted_zone_id" {
   default = "Z07887863W5D2X6RVH2WV"
 }  
  
-
   
