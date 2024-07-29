34680448e43692955ce8e42f92839b2a3d6a6fdc
prudhvi-1122 <prudhwi1998@gmail.com>
2024-07-29 18:20:51 +0530

    asw

--- infra-create/01-resource/ec2.tf
+++ infra-create/01-resource/ec2.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devrobo-s4"
+    bucket = "devrobop-s4"
     # key = "tools/terraform.tfstate"
     key = "test/key/terraform.tfstate"
     region = "us-east-1"
