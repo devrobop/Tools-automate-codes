be3f27626d61f75bb716ed2ac637ebbf01830971
prudhvi-1122 <prudhwi1998@gmail.com>
2024-07-29 18:25:27 +0530

    changes

--- state.tf
+++ state.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "devrobo-s4"
-    # key = "tools/terraform.tfstate"
-    key = "test/key/terraform.tfstate"
+    bucket = "devrobop-s4"
+    key = "tools/terraform.tfstate"
+    #key = "test/key/terraform.tfstate"
     region = "us-east-1"
   }
 }
