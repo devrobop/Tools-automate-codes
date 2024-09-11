ee95fb2d110814a54cf7f93d868de1d0ff5e3ffc
prudhvi-1122 <prudhwi1998@gmail.com>
2024-09-11 17:38:55 +0530

    recreate

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devrobop-s4"
+    bucket = "devrobop-s3"
     key = "tools/terraform.tfstate"
     #key = "test/key/terraform.tfstate"
     region = "us-east-1"
